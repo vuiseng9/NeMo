60ab30d86dcee949d9051fa1afa957911a1dbfce
Pablo Garay <palenq@gmail.com>
2025-11-10 19:25:50 -0800

    cp: `remove ExporDeploy` into `r2.6.0` (#15053)
    * chore: remove ExportDeploy (#15033)
    
    * add EP in PTQ (#15015)
    
    Signed-off-by: jenchen13 <jennifchen@nvidia.com>
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * remove ExportDeploy
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * remove exportDeploy tests
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * remove references
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * lintfix
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * Fixing lines for multispeaker pipeline (#15030)
    
    * Fixing lines for multispeaker pipeline
    
    Signed-off-by: taejinp <tango4j@gmail.com>
    
    * Removing unused imports
    
    Signed-off-by: taejinp <tango4j@gmail.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: tango4j <tango4j@users.noreply.github.com>
    
    * Making changes for HF Space deployment
    
    Signed-off-by: taejinp <tango4j@gmail.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: chtruong814 <chtruong814@users.noreply.github.com>
    
    * Updated multispk trans utils.
    
    Signed-off-by: taejinp <tango4j@gmail.com>
    
    ---------
    
    Signed-off-by: taejinp <tango4j@gmail.com>
    Signed-off-by: tango4j <tango4j@users.noreply.github.com>
    Signed-off-by: chtruong814 <chtruong814@users.noreply.github.com>
    Co-authored-by: tango4j <tango4j@users.noreply.github.com>
    Co-authored-by: chtruong814 <chtruong814@users.noreply.github.com>
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * remove ExportDeploy & references
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * lintfix
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * get load_ckpt back
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * lintfix
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: pablo-garay <pablo-garay@users.noreply.github.com>
    
    * back
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * revert back
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * revert back
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    * remove ExportDeploy
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    ---------
    
    Signed-off-by: jenchen13 <jennifchen@nvidia.com>
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    Signed-off-by: taejinp <tango4j@gmail.com>
    Signed-off-by: tango4j <tango4j@users.noreply.github.com>
    Signed-off-by: chtruong814 <chtruong814@users.noreply.github.com>
    Signed-off-by: pablo-garay <pablo-garay@users.noreply.github.com>
    Co-authored-by: Jenny Chen <jennifchen@nvidia.com>
    Co-authored-by: Taejin Park <tango4j@gmail.com>
    Co-authored-by: tango4j <tango4j@users.noreply.github.com>
    Co-authored-by: chtruong814 <chtruong814@users.noreply.github.com>
    Co-authored-by: pablo-garay <pablo-garay@users.noreply.github.com>
    
    * fix after ED remove (#15051)
    
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    
    ---------
    
    Signed-off-by: jenchen13 <jennifchen@nvidia.com>
    Signed-off-by: Pablo Garay <pagaray@nvidia.com>
    Signed-off-by: taejinp <tango4j@gmail.com>
    Signed-off-by: tango4j <tango4j@users.noreply.github.com>
    Signed-off-by: chtruong814 <chtruong814@users.noreply.github.com>
    Signed-off-by: pablo-garay <pablo-garay@users.noreply.github.com>
    Co-authored-by: Jenny Chen <jennifchen@nvidia.com>
    Co-authored-by: Taejin Park <tango4j@gmail.com>
    Co-authored-by: tango4j <tango4j@users.noreply.github.com>
    Co-authored-by: chtruong814 <chtruong814@users.noreply.github.com>
    Co-authored-by: pablo-garay <pablo-garay@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -82,12 +82,10 @@ extras_require = {
     'slu': req_file("requirements_slu.txt"),
     'multimodal-only': req_file("requirements_multimodal.txt"),
     'audio': req_file("requirements_audio.txt"),
-    'deploy': req_file("requirements_deploy.txt"),
-    'eval': req_file("requirements_eval.txt"),
 }
 
 
-extras_require['all'] = list(chain(val for key, val in extras_require.items() if key != 'deploy'))
+extras_require['all'] = list(chain(val for key, val in extras_require.items()))
 
 # Add lightning requirements as needed
 extras_require['common'] = extras_require['common-only']
@@ -117,7 +115,6 @@ extras_require['nlp'] = extras_require['nlp-only']
 extras_require['nlp'] = list(
     chain(
         extras_require['nlp'],
-        extras_require['eval'],
         extras_require['common'],
     )
 )
@@ -147,14 +144,6 @@ extras_require['slu'] = list(
     chain(
         extras_require['slu'],
         extras_require['asr'],
-    )
-)
-extras_require['deploy'] = list(
-    chain(
-        extras_require['nlp'],
-        extras_require['multimodal'],
-        extras_require['tts'],
-        extras_require['deploy'],
     )
 )
 
